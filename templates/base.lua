b3bd80563c896794a61db0efe52b867a7996c6cd
Michal Hoftich <michal.h21@gmail.com>
2026-06-01 16:28:36 +0200

    použij https pro schema.org

--- templates/base.lua
+++ templates/base.lua
@@ -255,9 +255,9 @@ local function template(data)
     -- }},
     h.footer{role="contentinfo",
       h.div{class="nav-container", 
-        div{class="column",itemtype="http://schema.org/Library",itemscope="",
+        div{class="column",itemtype="https://schema.org/Library",itemscope="",
           h["h3"]{T "Kontakt"},
-          div{h.address{itemprop="address",itemtype="http://schema.org/PostalAddress", h.span{ "Knihovna PedF UK"}, ", " , h.span {itemprop="streetAddress", "Magdaleny Rettigové 4, 116&#8239;39&nbsp;Praha&nbsp;1"}}},
+          div{h.address{itemprop="address",itemtype="https://schema.org/PostalAddress", h.span{ "Knihovna PedF UK"}, ", " , h.span {itemprop="streetAddress", "Magdaleny Rettigové 4, 116&#8239;39&nbsp;Praha&nbsp;1"}}},
           div{ h.img{src="/img/mail.svg", itemprop="email", alt="e-mail"}, a {href="mailto:knihovna@pedf.cuni.cz", "knihovna@pedf.cuni.cz"}},
           div{ h.img{src="/img/phone.svg", alt= T "telefon"}, a {href="tel:+420221900178",["aria-label"]="2 2 1 9 0 0 1 7 8",  h.span {itemprop="telephone", "+420 221 900 178"}}},
           div{ h.img {src= "/img/facebook.svg", alt= "facebook"}, a {href="https://www.facebook.com/knihovnapedfpraha", "Facebook"}}
